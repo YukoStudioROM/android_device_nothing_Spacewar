98375fa0b2d6746f916f4cef4c23c9227e1a6a6b
Bruno Martins <bgcngm@gmail.com>
2021-05-28 22:22:39 +0100

    Spacewar: Place volume panel on the left by default
    Change-Id: Iffcfad9af472ada48c0dc12cf238fd959355bc6a

--- device.mk
+++ device.mk
@@ -15,7 +15,8 @@ $(call inherit-product, vendor/nothing/Spacewar/Spacewar-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
