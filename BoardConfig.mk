eb7d24b51730c7900727794e94cefd37cf1b292e
Cosmin Tanislav <demonsingur@gmail.com>
2022-02-10 22:04:32 +0200

    Spacewar: hidl: fingerprint: Add UDFPS extension library
     * Mark the pressed fingerprint layer.
    
    Change-Id: I49891f89350839ff93ff1a762a83996bfd56ec18

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -94,6 +94,9 @@ TARGET_USES_ION := true
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
+# Fingerprint
+TARGET_SURFACEFLINGER_UDFPS_LIB := //$(DEVICE_PATH):libudfps_extension.nothing
+
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/configs/vintf/vendor_framework_compatibility_matrix.xml \
