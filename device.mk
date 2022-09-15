bc879377c8677d858ac8bbe9a9d03ba086201f33
Kujou Yuko <i@779.moe>
2022-09-15 09:24:18 +0000

    Spacewar: Import device specific overlays
     * Partially from LA.UM.9.14.r1-18900-LAHAINA.QSSI12.0.
     * Kang brightness-related values from Google Pixel 3a XL.
    
    Signed-off-by: Kujou Yuko <i@779.moe>
    Change-Id: I86fbbe54d8dde390ea212a64b936ff798131b6d9

--- device.mk
+++ device.mk
@@ -10,6 +10,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
@@ -125,3 +129,7 @@ PRODUCT_PACKAGES += \
     init.recovery.qcom.rc \
     init.target.rc \
     ueventd.qcom.rc
+
+# WiFi
+PRODUCT_PACKAGES += \
+    WifiResCommon
