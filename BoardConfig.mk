2f8bdb530d199d2106b5ecb7bf8dd57445df1fb8
Kujou Yuko <i@779.moe>
2022-09-15 13:15:06 +0000

    Spacewar: Import kernel module load list
    Signed-off-by: Kujou Yuko <i@779.moe>
    Change-Id: I411560f3efa22e8f05deea2fd69ffb820a24129b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -52,6 +52,10 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 TARGET_KERNEL_SOURCE := kernel/nothing/sm7325
 TARGET_KERNEL_CONFIG := vendor/lahaina-qgki_defconfig
 
+BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load))
+BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load.recovery))
+BOOT_KERNEL_MODULES := $(BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD)
+
 # Platform
 TARGET_BOARD_PLATFORM := lahaina
 
