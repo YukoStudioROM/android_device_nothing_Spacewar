71208cec50f36fc0b0b2fcac3c19afb9bb6b82b8
Kujou Yuko <i@779.moe>
2022-10-04 04:53:10 +0000

    Spacewar: Match device/bluetooth name with stock
    Signed-off-by: Kujou Yuko <i@779.moe>
    Change-Id: Idf184e9cc5584dc867c4a157b410b07e360ddea4

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -21,7 +21,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Nothing Phone (1)"
+#define BTM_DEF_LOCAL_NAME   "Nothing phone (1)"
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
 #define MAX_L2CAP_CHANNELS    32
