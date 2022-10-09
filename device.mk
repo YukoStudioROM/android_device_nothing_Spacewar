585a4f22d417e3b3de3f7e2c80c50d078edf878d
Kujou Yuko <i@779.moe>
2022-10-09 14:48:58 +0000

    Spacewar: Switch to OSS fingerprint HAL
    Signed-off-by: Kujou Yuko <i@779.moe>
    Change-Id: Iaba5f24935b65a0d7b315dcb5c1e903f249e357d

--- device.mk
+++ device.mk
@@ -206,7 +206,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3.vendor
+    android.hardware.biometrics.fingerprint@2.3-service.nothing
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
