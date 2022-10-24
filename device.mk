6a3dc197b23f2ca300a682f63b1b1f5761d50efa
Kujou Yuko <i@779.moe>
2022-10-24 17:48:12 +0800

    Spacewar: Build missing hostapd HIDL interface lib
    Signed-off-by: Kujou Yuko <i@779.moe>
    Change-Id: Ic57ccb9041f060c33dd4119f4e42090819304017

--- device.mk
+++ device.mk
@@ -411,6 +411,7 @@ PRODUCT_COPY_FILES += \
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
+    android.hardware.wifi.hostapd@1.0.vendor \
     hostapd \
     libwifi-hal-ctrl \
     libwifi-hal-qcom \
